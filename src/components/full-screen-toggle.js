cd93922ae546623d48eb6da9fa353aa88ece8eaa
Ethan Herr <eherr@reonomy.com>
2021-01-31 11:08:09 -0500

    handle esc

--- src/components/full-screen-toggle.js
+++ src/components/full-screen-toggle.js
@@ -1,9 +1,20 @@
 // hacky and bad DOM manipulator for img full-screen-toggle behavior within MD files
+
 const handleClick = (e) => {
   const wrapper = document.createElement('div');
+  function die() {
+    document.body.removeEventListener('keyup', handleKeyPress);
+    document.body.removeChild(wrapper);
+  }
+  function handleKeyPress(e) {
+    if (e.key === 'Escape') {
+      die()
+    }
+  }
   wrapper.className = 'full-screen-toggle-container';
   wrapper.appendChild(e.target.cloneNode());
-  wrapper.addEventListener('click', () => document.body.removeChild(wrapper));
+  wrapper.addEventListener('click', die);
+  document.body.addEventListener('keyup', handleKeyPress);
   document.body.appendChild(wrapper);
 }
 

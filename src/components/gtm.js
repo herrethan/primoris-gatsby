392b6546445152a1527db7b0644e51e99f72e4a4
Ethan Herr <eherr@reonomy.com>
2021-04-07 16:00:36 -0400

    correct react style attr

--- src/components/gtm.js
+++ src/components/gtm.js
@@ -11,9 +11,9 @@ const GTM = () => {
     <noscript>
       <iframe 
         src="https://www.googletagmanager.com/ns.html?id=GTM-KTX4DC2"
-        height="0"
-        width="0"
-        style="display:none;visibility:hidden">
+        height={0}
+        width={0}
+        style={{ display: "none", visibility: "hidden" }}>
       </iframe>
     </noscript>
   );
